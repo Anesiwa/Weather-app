0d1d2abef10cc12fb5affc317a6a77358d4bc7a6
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2021-10-17 20:26:19 -0500

    Revised JS file and spacing of HTML body

--- src/index.js
+++ src/index.js
@@ -52,7 +52,10 @@ function searchCurrentLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
